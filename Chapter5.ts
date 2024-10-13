faefd3f8ba3d0bb5fb6bfe312fe469774afb0e8e
KANGHO <ariaskkh@gmail.com>
2024-10-13 11:14:16 +0900

    constructor ref

--- Chapter5.ts
+++ Chapter5.ts
@@ -1,12 +1,12 @@
 function updateTile(x: number, y: number) {
   if (map[y][x].isStony() && map[y + 1][x].isAir()) {
-    map[y + 1][x] = new FallingStone();
+    map[y + 1][x] = new Stone(true);
     map[y][x] = new Air();
   } else if (map[y][x].isBoxy() && map[y + 1][x].isAir()) {
     map[y + 1][x] = new Fallingbox();
     map[y][x] = new Air();
   } else if (map[y][x].isFallingStone()) {
-    map[y][x] = new Stone();
+    map[y][x] = new Stone(false);
   } else if (map[y][x].isFallingBox()) {
     map[y][x] = new isBoxedPrimitive();
   }
@@ -27,11 +27,7 @@ class Air implements Tile {
 }
 
 class Stone implements Tile {
-  private falling: boolean;
-
-  constructor(falling: boolean) {
-    this.falling = falling;
-  }
+  constructor(private falling: boolean) {}
 
   isAir() {
     return false;
@@ -66,51 +62,6 @@ class Stone implements Tile {
         moveToTile(playerx + dx, playery);
       }
     } else if (this.isFallingStone() === true) {
-    }
-  }
-}
-
-class FallingStone implements Tile {
-  private falling: boolean;
-
-  constructor(falling: boolean) {
-    this.falling = falling;
-  }
-
-  isAir() {
-    return false;
-  }
-  isFallingStone() {
-    return this.falling;
-  }
-  isFallingBox() {
-    return false;
-  }
-  isLock1() {
-    return false;
-  }
-  isLock2() {
-    return false;
-  }
-  draw(g: CanvasRenderingContext2D, x: number, y: number) {}
-  moveVertical(dy: number) {}
-  isStony(): boolean {
-    return true;
-  }
-  isBoxy(): boolean {
-    return false;
-  }
-  moveHorizontal(dx: number) {
-    if (this.isFallingStone() === false) {
-      if (
-        map[playery][playerx + dx + dx].isAir() &&
-        !map[playery + 1][player + dx].isAir()
-      ) {
-        map[playery][playerx + dx + dx] = this;
-        moveToTile(playerx + dx, playery);
-      }
-    }
-    if (this.isFallingStone() === true) {
     }
   }
 }
