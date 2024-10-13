eb3f8291117a44337958e296b2cd8a69fdb2145b
KANGHO <ariaskkh@gmail.com>
2024-10-13 10:57:13 +0900

    stony, boxy 추가

--- Chapter5.ts
+++ Chapter5.ts
@@ -1,19 +1,27 @@
 function updateTile(x: number, y: number) {
-  if (
-    (map[y][x].isStone() || map[y][x].isFallingStone()) &&
-    map[y + 1][x].isAir()
-  ) {
+  if (map[y][x].isStony() && map[y + 1][x].isAir()) {
     map[y + 1][x] = new FallingStone();
     map[y][x] = new Air();
-  } else if (
-    (map[y][x].isBox() || map[y][x].isFallingBox()) &&
-    map[y + 1][x].isAir()
-  ) {
+  } else if (map[y][x].isBoxy() && map[y + 1][x].isAir()) {
     map[y + 1][x] = new Fallingbox();
     map[y][x] = new Air();
   } else if (map[y][x].isFallingStone()) {
     map[y][x] = new Stone();
   } else if (map[y][x].isFallingBox()) {
     map[y][x] = new isBoxedPrimitive();
+  }
+}
+
+interface Tile {
+  isStony(): boolean;
+  isBoxy(): boolean;
+}
+
+class Air implements Tile {
+  isStony(): boolean {
+    return false;
+  }
+  isBoxy(): boolean {
+    return false;
   }
 }
